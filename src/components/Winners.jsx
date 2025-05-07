17287aba2a6696cc4f5a5eb00fe4b24f0850325c
mr-dev-prince <pkcofficial24@gmail.com>
2025-05-07 18:47:07 +0530

    feat: implement optional chaining for safer access to election results in Winners component

--- src/components/Winners.jsx
+++ src/components/Winners.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import dummy from "../assets/dummy.jpeg";
 
 const Winners = ({ electionResult }) => {
-  
   if (electionResult.length < 1) {
     return null;
   }
@@ -26,35 +25,36 @@ const Winners = ({ electionResult }) => {
                 <p className="cal-sans text-2xl text-white">Results</p>
               </div>
               <div className="p-4 overflow-y-auto h-[calc(75vh-48px)] hide-scrollbar flex flex-col gap-4">
-                {election.resultsWithWinners.map((result) => (
-                  <div
-                    key={result.position}
-                    className="bg-gray-800/20 rounded-md flex justify-center items-center flex-col pb-6"
-                  >
-                    <h3 className="text-lg font-semibold text-white bg-gradient-to-r from-blue-500 to-purple-500 w-full text-center py-2 rounded-t-md">
-                      {result.position}
-                    </h3>
-                    <div className="border-2 relative rounded-md w-[200px] h-[250px] flex flex-col items-center justify-center mt-2 bg-gray-300/20">
-                      <div className="py-2 flex justify-center items-center h-[50%]">
-                        <img
-                          src={result.winner.student.profileImg || dummy}
-                          alt=""
-                          className="h-full rounded-full"
-                        />
+                {election?.resultsWithWinners.length > 0 &&
+                  election?.resultsWithWinners?.map((result) => (
+                    <div
+                      key={result.position}
+                      className="bg-gray-800/20 rounded-md flex justify-center items-center flex-col pb-6"
+                    >
+                      <h3 className="text-lg font-semibold text-white bg-gradient-to-r from-blue-500 to-purple-500 w-full text-center py-2 rounded-t-md">
+                        {result.position}
+                      </h3>
+                      <div className="border-2 relative rounded-md w-[200px] h-[250px] flex flex-col items-center justify-center mt-2 bg-gray-300/20">
+                        <div className="py-2 flex justify-center items-center h-[50%]">
+                          <img
+                            src={result?.winner?.student?.profileImg || dummy}
+                            alt=""
+                            className="h-full rounded-full"
+                          />
+                        </div>
+                        <div className="mt-2 flex flex-col gap-1 items-center text-white ">
+                          <p className="text-white font-semibold">
+                            {result?.winner?.student?.name}
+                          </p>
+                          <p>{result?.winner?.student?.email}</p>
+                          <p>Votes : {result?.winner?.votes}</p>
+                        </div>
+                        <span className="absolute -bottom-5 bg-green-600 w-[50%] py-1 text-white rounded-sm text-center cal-sans">
+                          Winner
+                        </span>
                       </div>
-                      <div className="mt-2 flex flex-col gap-1 items-center text-white ">
-                        <p className="text-white font-semibold">
-                          {result?.winner?.student?.name}
-                        </p>
-                        <p>{result?.winner?.student?.email}</p>
-                        <p>Votes : {result?.winner?.votes}</p>
-                      </div>
-                      <span className="absolute -bottom-5 bg-green-600 w-[50%] py-1 text-white rounded-sm text-center cal-sans">
-                        Winner
-                      </span>
                     </div>
-                  </div>
-                ))}
+                  ))}
               </div>
             </div>
           </div>
